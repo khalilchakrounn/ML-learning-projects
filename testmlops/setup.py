fa724405801b5213a9b9f6527e49503aaadb403a
khalilchakrounn <48619945+khalilchakrounn@users.noreply.github.com>
2024-03-13 12:08:56 +0100

    setup file added
    adding the setup file

---
+++ testmlops/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='votre_projet',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],  # Vos dépendances ici
+)
